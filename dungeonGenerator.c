8bb0755300f439565b375e2879eee3a8c81468d8
Guarionex <edwinom@iastate.edu>
2016-01-22 17:54:23 -0600

    Added seed base on time

--- dungeonGenerator.c
+++ dungeonGenerator.c
@@ -4,7 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-	
+	time_t seed = time(NULL);
+	srand(time);
 	printf("Random is %d\n", rand());
 	printf("Random is %d\n", rand());
 	printf("Random is %d\n", rand());
