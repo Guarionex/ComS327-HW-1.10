e3b3b2172e6754a6b026a41e50f6a870ddbfe22f
Guarionex <edwinom@iastate.edu>
2016-01-22 18:02:58 -0600

    added parameter for seed

--- dungeonGenerator.c
+++ dungeonGenerator.c
@@ -4,7 +4,16 @@
 
 int main(int argc, char *argv[])
 {
-	time_t seed = time(NULL);
+	time_t seed
+	if(argc != 2)
+	{
+		seed = time(NULL);
+	}
+	else if(argc == 2)
+	{
+		seed = atoi(argv[1]);
+	}
+	 
 	srand(seed);
 	printf("Random is %d\n", rand());
 	printf("Random is %d\n", rand());
