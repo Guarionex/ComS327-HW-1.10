867a7b3c46bd7b6e54c6e3166d6a7540d795ac17
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-01-23 17:37:16 -0600

    Change how I do rand() in room generator

--- dungeonRoomGenerator.c
+++ dungeonRoomGenerator.c
@@ -3,21 +3,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-Dungeon_Space_Room generateRoom(int *seed)
+Dungeon_Space_Room generateRoom()
 {	
-	int seed_local;
+	//int seed_local = seed;
 	
-	if(seed <= 0)
+	/*if(seed <= 0)
 	{
 		seed_local = time(NULL);
-		*seed = seed_local;
+		seed = seed_local;
 	}
 	else if(seed > 0)
 	{
-		seed_local = *seed;
-	}
+		seed_local = seed;
+	}*/
 	
-	srand(seed_local);
+	//srand(seed_local);
 
 	Dungeon_Space_Room room = Dungeon_Space_Room_create((rand()%4)+3, (rand()%4)+2); 
 	
@@ -42,13 +42,14 @@ Dungeon_Space_Room *generateMultipleRooms(int *seed)
 	srand(seed_local);
 	
 	num_room = (rand()%3)+5;
+	printf("Number of rooms is %d", num_room);
 	
 	Dungeon_Space_Room *room_collection = malloc(sizeof(Dungeon_Space_Room) * num_room);
 	Dungeon_Space_Room *room_collection_ptr;
 	
 	for(room_collection_ptr = room_collection; room_collection_ptr != room_collection + num_room - 1; room_collection_ptr++)
 	{
-		*room_collection_ptr = generateRoom(&seed_local);
+		*room_collection_ptr = generateRoom();
 	}
 	
 	return room_collection;
