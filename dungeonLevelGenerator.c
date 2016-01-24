2ae6aa3ce2063c8452e3273e9e1266d49f5d271b
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-01-24 01:46:41 -0600

    Fixed while loop condition

--- dungeonLevelGenerator.c
+++ dungeonLevelGenerator.c
@@ -45,7 +45,7 @@ bool Place_Rooms(Dungeon_Space_Room *rooms, int num_rooms)
 	{
 		printf("rooms[%d].x = %d\n", r, rooms[r].x);
 		printf("rooms[%d].y = %d\n", r, rooms[r].y);
-		while((rooms[r].x + rooms[r].width > 80) || (rooms[r].y + rooms[r].height > 21))
+		while(((rooms[r].x + rooms[r].width > 80) || (rooms[r].y + rooms[r].height > 21)) || (rooms[r].x < 0 || rooms[r].y < 0))
 		{
 			rooms[r].x = (rand()%78)+1;
 			rooms[r].y = (rand()%19)+1;
