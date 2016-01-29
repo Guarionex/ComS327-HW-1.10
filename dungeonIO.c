581212f0900ff563785d229e97f676d5565abe9e
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-01-29 00:13:46 -0600

    Made dungeonIO.c and typdef Flags enumeration and implement contains flag

---
+++ dungeonIO.c
@@ -0,0 +1,17 @@
+#include "dungeonIO.h"
+
+Flags *flags;
+
+bool Contains_Flag(Flags *flag, int size, Flags contains)
+{
+	int f;
+	for(f = 0; f < size; f++)
+	{
+		if(flag[f] == contains)
+		{
+			return TRUE;
+		}
+	}
+	
+	return FALSE;
+}
--- dungeonIO.h
+++ dungeonIO.h
@@ -1,7 +1,18 @@
 #ifndef DUNGEON_IO_H
 #define DUNGEON_IO_H
 
+#include "dungeonObjects.h"
 
+typedef enum 
+{
+	SEED,
+	SAVE, 
+	LOAD
+} Flags;
+
+extern Flags *flags;
+
+bool Contains_Flag(Flags *flag, int size, Flags contains);
 
 
 
