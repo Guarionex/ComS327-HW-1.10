9e49f7c34857f014a052fdd9e384c9982b097698
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-01-28 23:35:35 -0600

    Implementing argument inputs

--- dungeonGenerator.c
+++ dungeonGenerator.c
@@ -1,27 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "dungeonObjects.h"
 #include "dungeonRoomGenerator.h"
 #include "dungeonLevelGenerator.h"
 
+bool isstring(char *string);
+
 int main(int argc, char *argv[])
 {
 	time_t seed;
-	if(argc != 2)
+	if(argc < 2)
 	{
 		seed = time(NULL);
 	}
-	else if(argc == 2)
+	else if(argc >= 2)
 	{
-		seed = atoi(argv[1]);
+		int c;
+		for(c = 0; c < argc; c++)
+		{
+			if(isstring(argv[c]) == FALSE)
+			{
+				seed = atoi(argv[c]);
+				printf("seed = %d\n", seed);
+			}
+			else if(strstr(argv[c], "--") != NULL)
+			{
+				if(strcmp(argv[c], "--save") == 0)
+				{
+					printf("save\n");
+				}
+				else if(strcmp(argv[c], "--load") == 0)
+				{
+					printf("load\n");
+				}
+			}
+		}
 	}
-	srand(seed);
+	/*srand(seed);
 	
 	int int_seed = seed;
 	
 	Dungeon_Space_Struct **dungeon = Generate_Map(&int_seed);
-	Draw_Dungeon(dungeon);
+	Draw_Dungeon(dungeon);*/
 	
 	return 0;
+}
+
+bool isstring(char *string)
+{
+  while(*string != '\0')
+    {
+      if(isdigit(*string) == FALSE)
+	{
+	  return FALSE;
+	}
+      string++;
+    }
+  return TRUE;
 }
