e42cafb2e6d1260f5bf2205cc1c534b09d07da70
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-01-29 23:20:42 -0600

    trying freads

--- dungeonIO.c
+++ dungeonIO.c
@@ -22,10 +22,12 @@ Dungeon_Space_Struct **Load_Dungeon(char *file)
 	FILE *f;
 	f = fopen(file, "r");
 	char *header = malloc (6 * sizeof(char));
-	if(fgets(header, 6, f) != NULL)
+	/*if(fgets(header, 6, f) != NULL)
 	{
-		printf("Header is %s", header);
-	}
+		printf("Header is %s\n", header);
+	}*/
+	fread(header, sizeof (char), 6, f);
+    printf("Header is %s\n", header);
 	
 	Dungeon_Space_Struct **dungeon_map_load =  malloc(80 * sizeof(Dungeon_Space_Struct *));
 	int x;
@@ -38,6 +40,8 @@ Dungeon_Space_Struct **Load_Dungeon(char *file)
 			
 		}
 	}
+	
+	free(header);
 	
 	return dungeon_map_load;
 }
