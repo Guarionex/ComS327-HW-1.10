828f99d707e3d7a486f7721ee60affd2ca733f80
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-01-30 16:49:08 -0600

    testing file size error

--- dungeonIO.c
+++ dungeonIO.c
@@ -46,7 +46,7 @@ Dungeon_Space_Struct **Load_Dungeon(char *file)
 	printf("Hex headerH = 0x%lx\n", headerH);*/
 	if(items < 6 || strcmp(header, "RLG327") != 0)
 	{
-		printf("File is not in the correct format\n");
+		printf("File header is not in the correct format\n");
 		return 0;
 	}
     printf("Header is %s and read %d items\n", header, items);
@@ -56,24 +56,26 @@ Dungeon_Space_Struct **Load_Dungeon(char *file)
 	if(items < 4 || strcmp(version, "\x00\x00\x00\x00") != 0)
 	{
 		
-		printf("File is not in the correct format\n");
+		printf("File version is not in the correct format\n");
 		return 0;
 	}
 	printf("Version Marker is %s and read %d items\n", version, items);
 	
-	char *size = malloc( 4 * sizeof(char));
-	items = fread(size, sizeof(char), 4, f);
-	uint32_t sizeBE = *((uint32_t *) size);
-	printf("Hex sizeBE = 0x%x\n", sizeBE);
-	uint32_t sizeH = be32toh(sizeBE);
-	printf("Hex sizeH = 0x%x\n", sizeH);
-	/*if(items < 4 || strcmp(size, "\x00\x00\x00\x00") != 0)
+	char *sizeRaw = malloc( 4 * sizeof(char));
+	if((items = fread(sizeRaw, sizeof(char), 4, f)) < 4)
 	{
-		
 		printf("File is not in the correct format\n");
 		return 0;
-	}*/
-	printf("Size is %u and read %d items\n", sizeH, items);
+	}
+	uint32_t sizeBE = *((uint32_t *) sizeRaw);
+	//printf("Hex sizeBE = 0x%x\n", sizeBE);
+	uint32_t sizeH = be32toh(sizeBE);
+	//printf("Hex sizeH = 0x%x\n", sizeH);
+	if(sizeH < 1495)
+	{
+		printf("File is missing information\n");
+	}
+	printf("SizeRaw is %u and read %d items\n", sizeH, items);
 	
 	fclose(f);
 	free(header);
