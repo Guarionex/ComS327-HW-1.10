bb39eb2fa373721ba45f178213721439a493bb8a
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-01-30 14:23:10 -0600

    Reading header and version marker test

--- dungeonIO.c
+++ dungeonIO.c
@@ -33,13 +33,15 @@ Dungeon_Space_Struct **Load_Dungeon(char *file)
 			printf ("Other error\n");
 		}
 	}
+	
 	char *header = malloc (6 * sizeof(char));
-	/*if(fgets(header, 6, f) != NULL)
-	{
-		printf("Header is %s\n", header);
-	}*/
-	fread(header, sizeof (char), 6, f);
-    printf("Header is %s.\n", header);
+	int items = fread(header, sizeof(char), 6, f);
+    printf("Header is %s and read %d items\n", header, items);
+	
+	char *version = malloc( 4 * sizeof(char));
+	items = fread(version, sizeof(char), 4, f);
+	printf("Header is %s and read %d items\n", version, items);
+	
 	fclose(f);
 	free(header);
 	
