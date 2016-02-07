bb4d4b0c75f7cf86baa091cd397af24b88bdf2c6
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-02-07 06:08:30 -0600

    Made GameStateManager

---
+++ GameStateManager.c
@@ -0,0 +1,54 @@
+#include "GameStateManager.h"
+
+Dungeon_Space_Struct **current_dungeon;
+player_t pc;
+
+void Set_Dungeon(Dungeon_Space_Struct **dungeon)
+{
+	current_dungeon = dungeon;
+}
+
+void Set_Player(player_t player)
+{
+	pc = player;
+}
+
+void Draw_Dungeon(void)
+{
+	int x;
+	int y;
+	for(y = 0; y < 21; y++)
+	{
+		for(x = 0; x < 80; x++)
+		{
+			if(pc.pos.x == x && pc.pos.y == y;)
+			{
+				printf("@");
+			}
+			else
+			{
+				switch(current_dungeon[x][y].space_type)
+				{
+					case ROCK :
+					
+						if(current_dungeon[x][y].space_union.rock.density == 255)
+						{
+							printf("R");
+						}
+						else printf(" ");
+					
+					break;
+					case ROOM :
+					
+						printf(".");
+					
+					break;
+					case CORRIDOR :
+						printf("#");
+					break;
+				}
+			}
+		}
+		printf("\n");
+	}
+}
---
+++ GameStateManager.h
@@ -0,0 +1,14 @@
+#ifndef GAME_STATE_MANAGER_H
+#define GAME_STATE_MANAGER_H
+
+#include "dungeonObjects.h"
+#include "CharacterController.h"
+
+extern Dungeon_Space_Struct **current_dungeon;
+extern player_t pc;
+
+void Set_Dungeon(Dungeon_Space_Struct **dungeon);
+void Set_Player(player_t player);
+void Draw_Dungeon(void);
+
+#endif
