3e5127df1990ee1b51b24b743d9628b0328c8f1e
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-02-07 05:50:52 -0600

    Player Character done in CharacterController

--- CharacterController.c
+++ CharacterController.c
@@ -2,13 +2,47 @@
 
 player_t pc;
 
-int Place_Player(Dungeon_Space_Struct **dungeon, int seed)
+player_t Place_Player(Dungeon_Space_Struct **dungeon, int seed)
 {
-	for()
+	pos_t *open_pos = malloc(sizeof(pos_t));
+	open_pos[0] = NULL;
+	
+	int x, y, open_count = 0;
+	for(x = 0; x < 80; x++)
 	{
-		for()
+		for(y = 0; y < 21; y++)
 		{
-			
+			switch(dungeon[x][y].space_type)
+			{
+				case ROCK:
+				break;
+				
+				case ROOM:
+					pos_t new_pos;
+					new_pos.x = x;
+					new_pos.y = y;
+					open_count++;
+					open_pos = realloc(open_pos, sizeof(pos_t) + (sizeof(pos_t) * open_count));
+					open_pos[open_count-1] = new_pos;
+					open_pos[open_count] = NULL;
+				break;
+				
+				case CORRIDOR:
+					pos_t new_pos;
+					new_pos.x = x;
+					new_pos.y = y;
+					open_count++;
+					open_pos = realloc(open_pos, sizeof(pos_t) + (sizeof(pos_t) * open_count));
+					open_pos[open_count-1] = new_pos;
+					open_pos[open_count] = NULL;
+				break;
+			}
 		}
 	}
+	
+	srand(seed);
+	pc.pos = open_pos[rand()%open_count];
+	pc.cell = dungeon[pc.pos.x][pc.pos.y];
+	
+	return pc;
 }
--- CharacterController.h
+++ CharacterController.h
@@ -1,15 +1,16 @@
 #ifndef CHARACTER_CONTROLLER_H
 #define CHARACTER_CONTROLLER_H
 
+#include "dungeonObjects.h"
+
 typedef struct player
 {
 	Dungeon_Space_Struct cell;
-	int x;
-	int y;
+	pos_t pos;
 } player_t;
 
 extern player_t pc;
 
-int Place_Player(Dungeon_Space_Struct **dungeon, int seed);
+player_t Place_Player(Dungeon_Space_Struct **dungeon, int seed);
 
 #endif
