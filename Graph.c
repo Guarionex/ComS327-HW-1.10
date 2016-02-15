43ac04e281fc4a7970c3ef24019b1083ecece050
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-02-14 19:16:08 -0600

    Forgot to acces vertexData

--- Graph.c
+++ Graph.c
@@ -148,7 +148,7 @@ graph_t GenerateGraph(Dungeon_Space_Struct **dungeon, bool tunneler)
 				Add_Edge(internal_graph.vertices[v], NULL_VERTEX);
 				//printf("[%d][%d] is a border\n", internal_graph.vertices[v].x, internal_graph.vertices[v].y);
 			}
-			else if(!(tunneler == FALSE && Get_Vertex(internal_graph.vertices[v].x + a, internal_graph.vertices[v].y + b).space_type == ROCK))
+			else if(!(tunneler == FALSE && Get_Vertex(internal_graph.vertices[v].x + a, internal_graph.vertices[v].y + b).vertexData.space_type == ROCK))
 			{
 				Add_Edge(internal_graph.vertices[v], Get_Vertex(internal_graph.vertices[v].x + a, internal_graph.vertices[v].y + b));
 				//printf("Vertex at [%d][%d] is of weight = %d\n", internal_graph.vertices[v].x+a, internal_graph.vertices[v].y+b, Get_Vertex(internal_graph.vertices[v].x + a, internal_graph.vertices[v].y + b).weight);
