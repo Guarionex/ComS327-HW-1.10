8b817497beadcb50dc7c8db44ae6c66b2033356f
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-02-16 00:47:16 -0600

    Revert "made insert_vertex a pionter again"
    This reverts commit 3845714d3b00976c1af54f40b38c6305d2a80bcc.

--- Dijkstra.c
+++ Dijkstra.c
@@ -39,9 +39,9 @@ void Dijkstra(graph_t graph, vertex_t src)
 		int index_x = graph.vertices[v].x;
 		distance[v] = (Compare_Vertices(Get_Vertex(index_x, index_y), src) == TRUE) ? 0 : 428400;
 		predecessor[v] = NULL_VERTEX;
-		*vertex_t insert_vertex = malloc(sizeof(vertex_t));
+		vertex_t insert_vertex;// = malloc(sizeof(vertex_t));
 		insert_vertex = graph.vertices[v];
-		nodes[v] = binheap_insert(&h, insert_vertex);
+		nodes[v] = binheap_insert(&h, &insert_vertex);
 		
 	}
 	
@@ -67,7 +67,7 @@ void Dijkstra(graph_t graph, vertex_t src)
 		//free(adjacent_edges);
 	}
 	
-	free(nodes);
+	//free(nodes);
 	//free(insert_vertex);
 }
 
