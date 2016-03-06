8333c8ef33786e36a716abe327261342b3671f41
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-03-05 19:39:32 -0600

    finding priblem

--- Dijkstra.c
+++ Dijkstra.c
@@ -59,8 +59,14 @@ void Dijkstra(graph_t graph, vertex_t src)
 			vertex_t next = adjacent_edges[e].target;
 			uint p = distance[current->y*80+current->x] + next.weight;
 			uint q = distance[next.y*80+next.x];
-			printf("The values = %d", q);
-			printf("The index = %d", next.y*80+next.x);
+			if(q < 0 || q > 428400)
+			{
+				printf("The values = %d\n", q);
+			}
+			if(next.y*80+next.x < 0)
+			{
+				printf("The index = %d\n", next.y*80+next.x);
+			}
 			if(p < q/*(distance[current->y*80+current->x] + next.weight  < distance[next.y*80+next.x]) && (Contains_Vertex(visited, visited_count, next) == FALSE) && (Compare_Vertices(next, NULL_VERTEX) == FALSE)*/)
 			{
 				if((Contains_Vertex(visited, visited_count, next) == FALSE))
