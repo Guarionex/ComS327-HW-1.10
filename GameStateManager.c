e95edbed6e96766ce7ad576f1df94afb49c25abb
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-03-05 18:32:19 -0600

    freeing graph

--- GameStateManager.c
+++ GameStateManager.c
@@ -461,10 +461,12 @@ void turn(int *seed)
 		if(current->character_type == PLAYER)
 		{
 			free(distance_dungeon);
+			Destroy_Graph(&graphed_dungeon);
 			distance_dungeon = Generate_Distance_Dungeon(FALSE);
 			//Draw_Distance_Dungeon(distance_dungeon);
 			set_distance(distance_dungeon);
 			free(distance_dungeon_tunneler);
+			Destroy_Graph(&graphed_dungeon);
 			distance_dungeon_tunneler = Generate_Distance_Dungeon(TRUE);
 			set_distance_tunneler(distance_dungeon_tunneler);
 			update_telepath();
