79b3b266e514e5747bd8d77cc6e0b86650d13719
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-03-06 00:11:29 -0600

    testing commented out drawing

--- GameStateManager.c
+++ GameStateManager.c
@@ -595,8 +595,8 @@ void turn(int *seed)
 			line_of_sight(current_dungeon);
 			Draw_Dungeon(1);
 			//sleep(3);
-			int input = getch();
-			printf("Input is = %d\n", input);
+			//int input = getch();
+			//printf("Input is = %d\n", input);
 			if(character_list[0].alive == FALSE)
 			{
 				//printf("Player is dead\n");
