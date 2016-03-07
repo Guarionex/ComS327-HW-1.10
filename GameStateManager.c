3bda93db692bc5f2555331bafc794000f38a4c86
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-03-06 19:59:08 -0600

    error fix

--- GameStateManager.c
+++ GameStateManager.c
@@ -511,11 +511,11 @@ void Draw_Monster_List(void)
 	clear();
 	for(d = 0; d < strlen(debug_line); d++)
 	{
-		mvaddch(0, d, debug_len[d]);
+		mvaddch(0, d, debug_line[d]);
 	}
 	for(m = 1; m < num_characters; m++)
 	{
-		pos_t distance_to_player = {character_list[0].pos.x - character_list[m].pos.x, character_list[0].pos.y - character_list[m].pos.y}
+		pos_t distance_to_player = {character_list[0].pos.x - character_list[m].pos.x, character_list[0].pos.y - character_list[m].pos.y};
 		sprintf(monster_info[m-1], "%x: is %d %s and %d %s", character_list[m].character_parent.monster.abilities, (distance_to_player.x > 0)? distance_to_player.x : distance_to_player.x*-1,(distance_to_player.x > 0) ? "west" : "east", (distance_to_player.y > 0)? distance_to_player.y : distance_to_player.y*-1,(distance_to_player.y > 0) ? "north" : "south");
 		int c;
 		for(c = 0; c < strlen(monster_info[m-1]); c++)
