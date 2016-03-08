8dc906f0a0ab5c73903b826a4f4143ebe5d7b431
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-03-07 22:07:47 -0600

    coloring

--- GameStateManager.c
+++ GameStateManager.c
@@ -500,7 +500,7 @@ void Draw_Dungeon(int use_curses)
 	init_pair(1, COLOR_WHITE, COLOR_BLACK);
 	init_pair(2, COLOR_BLUE, COLOR_BLACK);
 	init_pair(3, COLOR_RED, COLOR_BLACK);
-	init_pair(4, COLOR_BLACK, COLOR_MAGENTA);
+	init_pair(4, COLOR_BLACK, COLOR_RED);
 	init_pair(5, COLOR_YELLOW, COLOR_BLACK);
 	init_pair(6, COLOR_BLACK, COLOR_GREEN);
 	init_pair(7, COLOR_BLACK, COLOR_YELLOW);
