7d1f1c489163443569acaca4da2067f62db5e1a5
Guarionex <edwinom@iastate.edu>
2016-03-08 21:46:09 -0600

    README and CHANGELOG finished, ready to submit

--- GameStateManager.c
+++ GameStateManager.c
@@ -592,7 +592,7 @@ void Draw_Dungeon(int use_curses)
 			}
 			if(character_list[0].pos.y == (d/81) && character_list[0].pos.x == d-((d/81)*81) && game_state == 2)
 			{
-				mvaddch((d/81)+1, d-((d/81)*81), '@');
+				mvaddch((d/81), d-((d/81)*81), '@');
 			}
 			else
 			{
