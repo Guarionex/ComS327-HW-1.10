9333d78a3e9c5dc6bd1af1cffc41ad39692f3b75
Guarionex <edwinom@iastate.edu>
2016-03-21 10:48:20 -0500

    making NULL_CHARACTER a const

--- CharacterController.h
+++ CharacterController.h
@@ -113,7 +113,7 @@ extern player_t *pc;
 extern int *character_map;
 extern character_t *character_list;
 extern int num_characters;
-extern character_t NULL_CHARACTER;
+extern const character_t NULL_CHARACTER;
 extern int dead_monsters;
 
 player_t *new_Player(void);
