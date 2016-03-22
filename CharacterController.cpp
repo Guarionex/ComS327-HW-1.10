b7ddd3d86fb664de635786327792efe3c7568dc4
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-03-22 02:39:10 -0500

    making Set_Player return a pointer

--- CharacterController.cpp
+++ CharacterController.cpp
@@ -194,7 +194,7 @@ void set_Character_all(character_t * toon, int32_t set_speed, int32_t new_time, 
 	((characterClass *) toon)->character_type = toon_type;
 }
 
-character_t Place_Player(Dungeon_Space_Struct **dungeon, int *seed)
+character_t *Place_Player(Dungeon_Space_Struct **dungeon, int *seed)
 {
 	pos_t *open_pos = (pos_t *) malloc(sizeof(pos_t));
 	open_pos[0] = NULL_POS;
