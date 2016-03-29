fce6c30c4e54854b61b9d9ebf05430623d06c597
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-03-29 01:16:10 -0500

    setting up flags

--- MonsterParser.cpp
+++ MonsterParser.cpp
@@ -8,9 +8,14 @@ vector<monsterClass> parseMonsters()
 	string file = "/.rlg327/monster_desc.txt";
 	string home = getenv("HOME");
 	ifstream monsterFile((home + file).c_str());
+	bool beginMonster = name = description = color = speed = ability = hp = damage = false;
 	
 	if(monsterFile.is_open())
 	{
+		if(line.compare("RLG327 MONSTER DESCRIPTION 1") != 0)
+		{
+			return monsterList;
+		}
 		while(getline(monsterFile, line))
 		{
 			cout << "line: " << line << "| comapre result = " << line.compare("RLG327 MONSTER DESCRIPTION 1") << endl;
@@ -18,10 +23,7 @@ vector<monsterClass> parseMonsters()
 			{
 				cout << "header same" << endl;
 			}
-			else
-			{
-				cout << "header not same" << endl;
-			}
+			
 		}
 		monsterFile.close();
 	}
