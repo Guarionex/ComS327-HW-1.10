e00faf71e20ad7374aa92583b384ec7a5a56162a
Guarionex <edwinom@iastate.edu>
2016-03-30 02:46:01 -0500

    testing

--- MonsterParser.cpp
+++ MonsterParser.cpp
@@ -131,7 +131,14 @@ vector<monsterClass> parseMonsters()
 					if(!speed)
 					{
 						//int base = -1, dice = -1, sides = -1;
-						string baseS = line.substr(line.find_first_not_of(" ", 6), line.find_first_of("+", 6) - 6);
+						try
+						{
+							int base = stoi(line.substr(line.find_first_not_of(" ", 6), line.find_first_of("+", 6) - 6));
+						}
+						catch(...)
+						{
+							//skip monster
+						}
 						cout << "Monster speed = " << baseS << endl;
 					}
 					else
