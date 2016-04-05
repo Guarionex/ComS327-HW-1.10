f3c1269e7464a7e2e386fc6b3b382a3a44899854
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-04-05 02:21:35 -0500

    testing

--- dungeonGenerator.cpp
+++ dungeonGenerator.cpp
@@ -347,7 +347,7 @@ int main(int argc, char *argv[])
 			character_t *player_to_set = Place_Player(dungeon, &int_seed);
 			//Set_Player(player_to_set);
 			Set_Debug_Info(int_seed, num_rooms);
-			populate_monsters(nummon_value, &int_seed);
+			populate_monsters(nummon_value, &int_seed, monsterList);
 			stair_t down;
 			if(stair_player == UPSTAIRS)
 			{
