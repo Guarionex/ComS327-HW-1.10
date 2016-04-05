1665b37ea4d4b32c62d3b30540267e454c3d41db
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-04-05 03:20:29 -0500

    testing

--- CharacterController.cpp
+++ CharacterController.cpp
@@ -306,7 +306,8 @@ character_t *create_monster(Dungeon_Space_Struct **dungeon, int *seed, vector<mo
 		useParsed = true;
 		parsedMonIndex = rand()%monsterList.size();
 		monsterToUse = monsterList[parsedMonIndex];
-		printf("Monster used = %s\n", monsterToUse.name.c_str());
+		//printf("Monster used = %s\n", monsterToUse.name.c_str());
+		cout << "Monster used = " << monsterToUse.name << endl;
 		//monster = new_Monster_existing(&monsterToUse);
 	}
 	uint8_t powers = 0b0000;
