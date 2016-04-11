ae522c126ba1f22ee3c7342f5ab117bc02baefa4
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-04-11 01:35:40 -0500

    fixing error

--- CharacterController.cpp
+++ CharacterController.cpp
@@ -946,7 +946,7 @@ boolean line_of_sight_helper(pos_t monster_pos, Dungeon_Space_Struct **dungeon)
 	return found_player;
 }
 
-boolean move_monster(character_t *player_to_move, Dungeon_Space_Struct **dungeon)
+string move_monster(character_t *player_to_move, Dungeon_Space_Struct **dungeon)
 {
 	pos_t move_to = {.x = get_Character_pos(player_to_move).x, .y = get_Character_pos(player_to_move).y};
 	boolean moving = FALSE, erratic = FALSE;
