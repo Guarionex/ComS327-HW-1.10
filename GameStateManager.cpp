13fda965f216f5a504c66d41ee5dded5656da71b
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-04-12 23:47:21 -0500

    testing

--- GameStateManager.cpp
+++ GameStateManager.cpp
@@ -1417,7 +1417,7 @@ int menu_helper(int menu_type, int commandInput, pos_t *moving_to)
 			{
 				continue;
 			}
-			if(commandInput == 16 || commandInput == 17 || commandInput == 9/* || input == 18 || input == 19 || input == 20 || input == 21 || input == 22*/)
+			if(commandInput == 16 || commandInput == 17 || commandInput == 9 || commandInput == 18 || commandInput == 19 || commandInput == 20 || commandInput == 21 || commandInput == 22)
 			{
 					commandInput = menu_helper(commandInput, commandInput, moving_to);
 			}
