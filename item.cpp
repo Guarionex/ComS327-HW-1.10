c10998f2d8cd882b5443df9ab84a09fc243a4d4e
Edwin O. Martinez Velazquez <edwinom@iastate.edu>
2016-04-12 22:30:06 -0500

    fixing error

--- item.cpp
+++ item.cpp
@@ -139,7 +139,7 @@ void removeItemAt(vector<itemClass> *search, int x, int y, string nameToRemove)
 	uint s;
 	for(s = 0; s < search->size(); s++)
 	{
-		if(search[s]->pos.x == x && search[s]->pos.y == y && search[s]->name.compare(nameToRemove) == 0)
+		if((*search)[s].pos.x == x && (*search)[s]pos.y == y && (*search)[s].name.compare(nameToRemove) == 0)
 		{
 			search->erase(search->begin() + s);
 			break;
